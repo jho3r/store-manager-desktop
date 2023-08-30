891c76fa52d7a5ab202060fe4d85928ddcaa5436
jho3r <jhoer.perez@gmail.com>
2023-08-30 00:28:40 -0500

    Fix max date problem

--- src/components/home/HomeHeader/HomeHeader.js
+++ src/components/home/HomeHeader/HomeHeader.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react'
 import styles from './HomeHeader.module.css'
 
-const HomeHeader = ({ salesDate, onDateChange, total, owed }) => {
+const HomeHeader = ({ salesDate, onDateChange }) => {
+  const [maxDate, setMaxDate] = useState(new Date().toLocaleDateString('en-CA'))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMaxDate(new Date().toLocaleDateString('en-CA'))
+    }, 60000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   const handleDateChange = (event) => {
     if (onDateChange) {
       onDateChange(event.target.value)
@@ -21,7 +32,7 @@ const HomeHeader = ({ salesDate, onDateChange, total, owed }) => {
             value={salesDate}
             onChange={handleDateChange}
             min="2023-06-07"
-            max={new Date().toLocaleDateString('en-CA')}
+            max={maxDate}
           />
         </div>
         <div className="col-12">
